bd889c8dedf148235f69c09ef997652c1c694b28
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-19 06:27:09 -0300

    Separates Footer to individual component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{
+      textAlign: 'center',
+      padding: '1rem',
+      fontSize: '0.9rem',
+      backgroundColor: '#FFFFFF'
+    }}>
+      Contact: loveguessr@example.com
+    </footer>
+  );
+}
+
+export default Footer;
